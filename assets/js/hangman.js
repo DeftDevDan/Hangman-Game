e9368e584882769fe5ca7023bea27bb8e6a6b0e9
Daniel Kang <mrdanielkang@gmail.com>
2017-02-09 01:17:28 -0800

    Finished Javascript

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,7 +1,7 @@
 var word = ["RickAndMorty", "TheWalkingDead", "Batman", "Superman", "Flash", "HarryPotter", "Robocop", "StarWars", "StarTrek", "HarleyQuinn", "Deadpool", "BackToTheFuture"];
 var wins = 0;
 var lives = 15;
-var guessed;
+var guessed = "";
 var displayWord = "";
 var randWord;
 var randNum;
@@ -9,44 +9,51 @@ var randNum;
 function loadFunction() {
 	randNum = Math.floor(Math.random()*10);
 	randWord = word[randNum];
+	displayWord = "";
+	lives = 15;
+	guessed= "";
 	wordHider(randWord);
 	document.getElementById('timesWon').innerHTML = wins.toString();
 	document.getElementById('randomWord').innerHTML = displayWord;
-	document.getElementById('userGuesses').innerHTML = randWord.length;
-	document.getElementById('userGuessed').innerHTML = randWord;
-	main();
-	//var userText = document.getElementById("timesWon");
-	//document.onkeyup = function() {
-	//userText.textContent = event.key;
-	//displayWord= event.key;
-	//String.event.key.fromCharCode(event.key).toLowerCase()
-	//}
-}
+	document.getElementById('userGuesses').innerHTML = lives;
+	document.getElementById('userGuessed').innerHTML = guessed;
+};
 
 function wordHider(x) {
 	for (var i = 0; i < randWord.length; i++) {
-		displayWord += "_ ";
+		displayWord += "-";
 	}
-}
+};
 
-function main() {
-	var wins = document.getElementById ("timesWon");
-	var lives = document.getElementById ("userGuesses");
-	var guessed = document.getElementById ("userGuessed");
-	document.onkeyup=function(){
-		while(lives > 0) {
-			if (randWord.indexOf() > -1) {
-				alert("You Guessed It");
-			}
-		
+document.onkeyup=function() {
+	if (lives > 0) {
+		if (guessed.toLowerCase().indexOf(event.key) > -1) {
+			
+		} else if (randWord.toLowerCase().indexOf(event.key) > -1) {
+			replace();
+		} else {
+			guessed += event.key + " ";
+			document.getElementById('userGuesses').innerHTML = guessed;
+			lives -= 1;
+			document.getElementById('userGuessed').innerHTML = lives;
+		}
+	} else {
+		alert("Sorry, you lost. The word was: " + randWord);
+		loadFunction();
+	}
+};
+
+function replace() {
+	for (var i = 0; i < randWord.length; i++) {
+		if (randWord.toLowerCase().charAt(i) === event.key) {
+			displayWord = displayWord.substr(0, i) + randWord.charAt(i) + displayWord.substr(i+1);
 		}
 	}
-}
-
-function GetIndex(val, e) {
-	alert(val.toLowerCase().indexOf(String.fromCharCode(e.keyCode).toLowerCase()));
-}
-
-String.prototype.replaceAt=function(index, character) {
-    return this.substr(0, index) + character + this.substr(index+character.length);
+	document.getElementById('randomWord').innerHTML = displayWord;
+	if (randWord.toLowerCase() == displayWord.toLowerCase()) {
+		wins += 1;
+		document.getElementById('timesWon').innerHTML = wins.toString();
+		alert("Congrats! You won! The word was: " + randWord);
+		loadFunction();
+	}
 }
